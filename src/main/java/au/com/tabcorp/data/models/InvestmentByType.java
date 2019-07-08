c08c25371d4381bf81dcd77caa9e23134234e815
Madhu Prasad Paridi <madhuprasadparidi@gmail.com>
2019-07-08 11:29:10 +1000

    Create InvestmentByType.java

---
+++ src/main/java/au/com/tabcorp/data/models/InvestmentByType.java
@@ -0,0 +1,37 @@
+package au.com.tabcorp.data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class InvestmentByType implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String betType;
+	BigDecimal totalInvestment;
+	public BigDecimal getTotalInvestment() {
+		return totalInvestment;
+	}
+	public void setTotalInvestment(BigDecimal totalInvestment) {
+		this.totalInvestment = totalInvestment;
+	}
+	public String getBetType() {
+		return betType;
+	}
+	public void setBetType(String betType) {
+		this.betType = betType;
+	}
+
+	public InvestmentByType(String betType, BigDecimal totalInvestment ) {
+		super();
+		this.totalInvestment = totalInvestment;
+		this.betType = betType;
+	}
+	public InvestmentByType() {
+		super();
+
+	}
+
+}
